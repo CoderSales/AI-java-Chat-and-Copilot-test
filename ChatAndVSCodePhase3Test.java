c495ccdfdf1cfac7ad6266f24e855057e49e6f8b
Coder731 <stephenobrien1@gmail.com>
2023-12-07 23:44:34 +0000

    Bug to be deprecated Chat java not working

---
+++ ChatAndVSCodePhase3Test.java
@@ -0,0 +1,30 @@
+public class ChatAndVSCodePhase3Test {
+    private int number;
+
+    public int PrimeChecker(int number) {
+        this.number = number;
+    }
+
+    public boolean isPrime() {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int numToCheck = 17;  // Replace with the number you want to check
+        PrimeChecker primeChecker = new PrimeChecker(numToCheck);
+
+        if (primeChecker.isPrime()) {
+            System.out.println(numToCheck + " is prime.");
+        } else {
+            System.out.println(numToCheck + " is not prime.");
+        }
+    }
+}
